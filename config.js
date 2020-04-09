6214865f8777c54cbf65e17b341842882ac10a9f
lavanya <gundu.lavanya@gmail.com>
2020-04-09 00:35:13 -0700

    passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : ' mongodb://localhost:27017/nucampsite',
+    'facebook' : {
+        clientId: '171973977186991',
+        clientSecret: '55804325f3c1f9ac6dc7e64cab69993c'
+    }
 }
